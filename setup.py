91c6490a465c700b6980d5388ce3cb89389d4b04
Shivanshu2407 <st955218@gmail.com>
2025-06-06 11:04:05 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Shivanshu Tiwari",
+    author_email="st955218@gmail.com",
+    packages=find_packages()
+)
